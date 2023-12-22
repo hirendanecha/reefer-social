0d362e0331ec2a391f35ad028f3b52ab6b74f5e4
vatsal <vso.opash@gmail.com>
2023-12-22 18:15:13 +0530

    Change images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ import { Howl } from 'howler';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'healing-tube';
+  title = 'Reefer.social';
   showButton = false;
   tab: any;
 
